54836d3ef465dfe10b1d609d4b8d7888c0bc45b2
Sid <rajanraghav495@gmail.com>
2026-05-21 13:02:57 +0530

    url update
    url update

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,6 @@ export default defineConfig(({mode}) => {
       // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
-    base: '/MP-Fiber',
+    base: '/MP-Fiber/',
   };
 });
